d95b0f866e1733d285817154cba162d62fabc171
josephd <joe.robotics@hotmail.com>
2014-04-30 09:44:31 -0400

    Create Joystick.h

---
+++ Arduino/Joystick.h
@@ -0,0 +1,13 @@
+#ifndef JOYSTICK_H
+#define JOYSTICK_H
+
+#include "Arduino.h"
+
+class Joystick
+{
+public:
+  static void begin();
+  static void sendData(float xvel, float yvel, boolean leftclick, boolean rightclick, boolean crouch, boolean w, boolean a, boolean s, boolean d);
+};
+
+#endif
